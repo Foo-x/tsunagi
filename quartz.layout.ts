c84f14d18a295e3b398e925fe9be5261b5453481
Foo-x <www.sand.x@gmail.com>
2025-02-24 02:04:31 +0900

    Quartz sync: Feb 24, 2025, 2:04 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,7 +18,9 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    Component.ContentMeta({
+      showReadingTime: false,
+    }),
     Component.TagList(),
   ],
   left: [
@@ -28,10 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.Explorer(),
   ],
-  right: [
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  right: [Component.DesktopOnly(Component.TableOfContents()), Component.Backlinks()],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
